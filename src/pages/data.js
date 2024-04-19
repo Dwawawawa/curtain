790d02d7e743885b7dc2a2d08eb2f6aa88e46ad7
JANG HYO JE <113086153+Dwawawawa@users.noreply.github.com>
2024-04-19 21:09:10 +0900

    feat : data.js 풍경을 변환

--- src/pages/data.js
+++ src/pages/data.js
@@ -2,27 +2,99 @@ let data =
 [
     {
         id:0,
-        src : "https://i.namu.wiki/i/vffbNmI7-PbYBpzdflYGuEzt5k73rkM_tTfCPyKskgNolPHFZrzYAHLMcw4v-iaGjNfRXFlIV-X9_7kwfAJ_RFVQlBln27OZCWal5Duh15UOofzASXA9oy2GQBxx1hLT5yJBHD4luSj8uhaun73kaw.webp"
+        src : "https://postfiles.pstatic.net/MjAyNDA0MTlfMjc4/MDAxNzEzNTI0Mzg3NDMz.y3AWCNlMU_vkDR5z-KJfMQFQrAmi4q_TflLMB0hHVlkg.5CZ_vkTcuAaTM5LvajxhD5XrfuQ-pJP6gumuJxRjf5Ig.JPEG/KakaoTalk_20240419_182245502.jpg?type=w466"
     },
     {
         id:1,
-        src: "https://i.namu.wiki/i/YpsNG06XD07WNe8G0X9nOolbjzrz27lNv_r0gqDGcOTsZ5y8Kj5S5YXf4KwKDPeLAgnKMwQP_UUdJ84nfchbaNeNhX358hVCYDM5CFi16N8I4uLt3IioDdzoJvNdLc8--3gjKEebXeGP5M-_8vEmOg.webp"
+        src: "https://postfiles.pstatic.net/MjAyNDA0MTlfMjA4/MDAxNzEzNTI0Mzg3MzI3.8siRWqDpBJmzL6TNiFLoPh9c17NYf8obcZk1ZD_6ABcg.KFzc2804ilIZhImaJlU5p3hBEUgsTc60Tt9SPH7y92og.JPEG/KakaoTalk_20240419_182245502_01.jpg?type=w466"
     },
     {
         id:2,
-        src: "https://i.namu.wiki/i/2pQvC1M_KpG0W_QRhy64newGeFgh49cDnDVopvviyTdKCupJ2qNkTpWG6nVkSoIzOOaH_raaJhRWzFaa-GoWei_THf_U2zD-PDenPSh-MEEwV6LWVB9ZRvd4_y8wDDs14ngdhYYH0iqmS-F--ZLJUA.webp"
+        src: "https://postfiles.pstatic.net/MjAyNDA0MTlfMTY2/MDAxNzEzNTI0Mzg3Mzc4.tXyyvRRw0HjmSdlVmov4gZS0Rj4j1D8f7eH6ouciEYIg.IPAZk3OpX-yLay5rBaorZ9rsSLP63C3mRc3DKKZoOL0g.JPEG/KakaoTalk_20240419_182245502_02.jpg?type=w466"
     },
     {
         id:3,
-        src: "https://i.namu.wiki/i/Sgg6AHqTXnLWYmfJYR5ma_YzKyM-26mXS1Kvo7sZfRniPXKPvSHATqUKKbJcctX629UbabaomfuCdOEMa6pAicRnOFuUidage5TDFzYAW1I00KYtsTZPxekG0Z8q5MEEBNKGsLRe6ABAIeRtooxJYg.webp"
+        src: "https://postfiles.pstatic.net/MjAyNDA0MTlfMTM2/MDAxNzEzNTI0Mzg3NDYx.VdxYH6d4flVP9zgEqnZg-48QgwusftN_GMGAyPGsp3sg.kjLvHkdY-NREjhw4jiWHR0kG5e6Hbst-of73wmzweq4g.JPEG/KakaoTalk_20240419_182245502_03.jpg?type=w466"
     },
     {
         id:4,
-        src: "https://i.namu.wiki/i/Ox2uwo_HoQtYG_RNtsijVYYYXqx1diowLa9B72yF1rDsHEOyBI6zPNaii8BBq3YmS7go1RYLFCdUpK6_WyMpLADHVX6eE-68yjjXuOqCQydkwb5XwHzM99Vh-E7nmegHGL7Z7PTLj4ae4HlG5E4yRA.webp"
+        src: "https://postfiles.pstatic.net/MjAyNDA0MTlfMTIw/MDAxNzEzNTI0NDAzMTEx.YV7SexwM1CXtoIu3UZS5eX0wNnOggHb1d4sg9nX3YBEg.kH4zkjQ2megDQ2xk2XFsIz5fmiEyqBxuw1G2pxNEM4og.JPEG/KakaoTalk_20240419_182245502_04.jpg?type=w466"
     },
     {
         id:5,
-        src: "https://i.namu.wiki/i/hU9NKWUsqln0hDwqMKxU1CnKN-8hq9eWhQOrqAoLktFxNlBKIUsJBPUBvsNIwtNMegIgW-gKKKHj1IQN0syZ0nRUoT73VUNE6fM3fwda-vQwk3LRnT_DPvqDKpIJd2OYNIG4u2WtFPegiPWSdAiUrg.webp"
+        src: "https://postfiles.pstatic.net/MjAyNDA0MTlfMTA4/MDAxNzEzNTI0NDAzMzM2.txn3DQ8WMMFdjP6Cs4kEHKA4eLppjeJtz7ulbzOQ2hkg.Vz5Hqf9rUiWzz_eUNiyFnGX17xmnjWcQ91U8MTlsEH4g.JPEG/KakaoTalk_20240419_182245502_05.jpg?type=w466"
+    },
+    {
+        id:6,
+        src : "https://postfiles.pstatic.net/MjAyNDA0MTlfMjA3/MDAxNzEzNTI0NDAzMTA5.bE0aVC48dDE_xOwPtcJvKM64n6KD4xFAkYVx6A5L8mAg.rkwtqFtawEDgN9r2SSSXimLVk84z6t5-ZYWEE1sTJxQg.JPEG/KakaoTalk_20240419_182245502_06.jpg?type=w966"
+    },
+    {
+        id:7,
+        src: "https://postfiles.pstatic.net/MjAyNDA0MTlfMjEw/MDAxNzEzNTI0NDAzMjgx.6mbJ58lk9Jd4-teWMKS5Bk6tRtqbXKyuuB9k5UB6F-0g.2S4dq1rBTOI_MwDYG0JnWk4NQ649HfP6B6V2VZRIPg4g.JPEG/KakaoTalk_20240419_182245502_07.jpg?type=w466"
+    },
+    {
+        id:8,
+        src: "https://postfiles.pstatic.net/MjAyNDA0MTlfODcg/MDAxNzEzNTI0NDE5MjY0.OgpMC9wMo4cJ17QBHi2yrBNVd8kFT_wKoGos-0zvkGwg.dg0rcPydpr1vMRFFMOTUxQcNW_Wjiz0aXC8RI5uEbukg.JPEG/KakaoTalk_20240419_182245502_08.jpg?type=w466"  
+    },
+    {
+        id:9,
+        src: "https://postfiles.pstatic.net/MjAyNDA0MTlfMjU5/MDAxNzEzNTI0NDE5NDI3.f-4bgX9StWpkpMl9xsWhDvIjdr8F9uyxQ1YNwcTt6qog.l8Ij289B_-M1sHuuP7Yglxa33tUD8YLSJkhpkYbw73gg.JPEG/KakaoTalk_20240419_182245502_09.jpg?type=w466"      
+    },
+    {
+        id:10,
+        src: "https://postfiles.pstatic.net/MjAyNDA0MTlfMjg1/MDAxNzEzNTI0NDE5MjYy.fwXPE_BDczLzs8ZPNl9KxcfKGtoSA196iU3FyRtdTmQg.UguEJMqO7dg5IVTHG37SI4qNzv2obQCBJm3YqEqAfZgg.JPEG/KakaoTalk_20240419_182245502_10.jpg?type=w466"
+    },
+    {
+        id:11,
+        src : "https://postfiles.pstatic.net/MjAyNDA0MTlfMTUw/MDAxNzEzNTI0NDE5NDQ3.-cqvAtSAFIQCkg7AoNlc8bGCXMCDzk-zSm7ayZOrKiog.r9DaM6mFsbKjP6C3hAfnJlbKkYfwvD20s-p3J3U9DeEg.JPEG/KakaoTalk_20240419_182245502_11.jpg?type=w466"
+    },
+    {
+        id:12,
+        src: "https://postfiles.pstatic.net/MjAyNDA0MTlfMTcy/MDAxNzEzNTI0NDQ2NDA1.fmN5JEBNoJWVGPamq9Am1hdGuv0jjNMn96AXn90Xc0sg.vY4QXSC55YN79Xj6hY3TqLN_-h0fd257LxxM_evjNJMg.JPEG/KakaoTalk_20240419_182245502_12.jpg?type=w466"
+    },
+    {
+        id:13,
+        src: "https://postfiles.pstatic.net/MjAyNDA0MTlfOTYg/MDAxNzEzNTI0NDQ2NDY2.gz-sSSEtlnJyTUNljrlsbWhbQa3uY66TYOTbx__hii4g.ViDEvfQFu0P2xh7Co-h-YvymQgNQRaCnrcDAkqu4vvUg.JPEG/KakaoTalk_20240419_182245502_13.jpg?type=w466"        
+    },
+    {
+        id:14,
+        src: "https://postfiles.pstatic.net/MjAyNDA0MTlfMjgz/MDAxNzEzNTI0NDQ2NDY3.jtg2u8ewx0KwrXGzaPkA7DoKicAbpAxiP-orq1XvZo8g.6nQvU55NMBVHnvPgXXBgAIDORG0t_HWwABXhttbhQ48g.JPEG/KakaoTalk_20240419_182245502_14.jpg?type=w466"
+    },
+    {
+        id:15,
+        src: "https://postfiles.pstatic.net/MjAyNDA0MTlfMTE0/MDAxNzEzNTI0NDQ2Mzc5.E6ZiaMOOcsZHO3lC-UXrLw030fCrNXGTGaw6cushDYUg.KvYAUeqP-IkZzQwX-GLeeXmG-1J7-FLZMnk9oZU5cr0g.JPEG/KakaoTalk_20240419_182245502_15.jpg?type=w466"
+    },
+    {
+        id:16,
+        src: "https://postfiles.pstatic.net/MjAyNDA0MTlfNzQg/MDAxNzEzNTI0NDczNDgx.KO_zRJteadbsB1r_sJSt1bOzq-KwB9Diseey5Y78fE4g.Bvg0np3bv3Y696EF6dju6e7CtLzEKw9t9hraZEbv-U8g.JPEG/KakaoTalk_20240419_182245502_16.jpg?type=w466"
+    },
+    {
+        id:17,
+        src: "https://postfiles.pstatic.net/MjAyNDA0MTlfMTc1/MDAxNzEzNTI0NDczMzkx.K7Ysu2-U08xqgD1924Bts1Fvn_7XJAqhdRsILDXygGog.z-HVjFcOW5ly0voMTvhFZqIobLn03eHANj8jU4XAuqAg.JPEG/KakaoTalk_20240419_182245502_17.jpg?type=w466"
+    },
+    {
+        id:18,
+        src : "https://postfiles.pstatic.net/MjAyNDA0MTlfMjc5/MDAxNzEzNTI0NDczNDk2.S03qnbFOMUSsWHBN3A9QhMt0YH2gr6Vmye6eYAXq22Qg.yGR6Oie1TDPMiIi-huudodEMkG4q8PgBSTSyOqRjyEgg.JPEG/KakaoTalk_20240419_182245502_18.jpg?type=w466"
+    },
+    {
+        id:19,
+        src: "https://postfiles.pstatic.net/MjAyNDA0MTlfMjk4/MDAxNzEzNTI0NDczNDAz.0AmrBZ3AMvkaY2Wt5794aK-YtgEgHWrxBrYqJn2WSfAg.D8dLXNvb2KhZCBY4uRAzsDMP3Qj2R0cqp1l-vA8h_rgg.JPEG/KakaoTalk_20240419_182245502_19.jpg?type=w466"
+    },
+    {
+        id:20,
+        src: "https://postfiles.pstatic.net/MjAyNDA0MTlfMTgz/MDAxNzEzNTI0NDg4MTA2.T7LcFjf3rHFhSYZbKdb8yWLSNHZ9U6IZEais86bkfu8g.A7z-m1Id7exGB53zVOJ-2dBD9LCvbxY6OQ-m20uBqIcg.JPEG/KakaoTalk_20240419_182245502_20.jpg?type=w466"
+    },
+    {
+        id:21,
+        src: "https://postfiles.pstatic.net/MjAyNDA0MTlfMzEg/MDAxNzEzNTI0NDg4Njc0.we72m3blL521L4nCbWxrhiflvED3zE4ERLaOGOQZMx0g.n93PqAHTDNQ0YnMBrmExMwSpnRCW0m6rNt52RqPiQGIg.JPEG/KakaoTalk_20240419_182245502_21.jpg?type=w466"
+    },
+    {
+        id:22,
+        src: "https://postfiles.pstatic.net/MjAyNDA0MTlfMjkx/MDAxNzEzNTI0NDg4NTg3.fvVZ1OkPeNMN4VjO86S1uMlZaVjoO5DKWHwPI3ndZvQg.o7K0YgRB1hU6P_HNXDHeP5AnZnS6u5Uvp0Ij7KIxWv0g.JPEG/KakaoTalk_20240419_182245502_22.jpg?type=w466"
+    },
+    {
+        id:23,
+        src: "https://postfiles.pstatic.net/MjAyNDA0MTlfMzQg/MDAxNzEzNTI0NDg4NTMz.geaLMSgbTUOhH8mkmNCsr0LM8XBiXYu2WKKomtkQOywg.iqSQfGvNf0OGxzFIE3nxGNSB1wiOX5j7d46atnbsChsg.JPEG/KakaoTalk_20240419_182245502_23.jpg?type=w466"
     }
 
 ]
